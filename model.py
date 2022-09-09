238d24160b458ce89e5d78fa25c510ad73afc8ea
Hana Zait <hana.o.zait@gmail.com>
2022-09-09 16:03:57 -0700

    Completed tests in tests.py

--- model.py
+++ model.py
@@ -21,7 +21,10 @@ def connect_to_db(app, db_uri="postgresql:///games"):
 def example_data():
     """Create example data for the test database."""
     # FIXME: write a function that creates a game and adds it to the database.
-    print("FIXME")
+    game_1 = Game(name ="Settlers of Cataan", description="a multiplayer board game")
+    db.session.add(game_1)
+    db.session.commit()
+
 
 
 if __name__ == '__main__':
